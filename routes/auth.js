d15a69d7efb75e3fbf29ba8670977af385fb866b
Maximilian <scientificresearch309@gmail.com>
2023-09-18 23:48:33 +0200

    and Now, our Token has all the information stored in MongoDB, proved in jwt.io website

--- routes/auth.js
+++ routes/auth.js
@@ -24,7 +24,15 @@ router.post("/", async (req, res) => {
   const validPassword = await bcrypt.compare(req.body.password, user.password);
   if (!validPassword) return res.status(400).send("Invalid email or password");
 
-  const token = jwt.sign({ _id: user._id }, "jwtPrivateKey");
+  const token = jwt.sign(
+    {
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      password: user.password,
+    },
+    "jwtPrivateKey"
+  );
   // res.send(true);
   res.send(token);
 
