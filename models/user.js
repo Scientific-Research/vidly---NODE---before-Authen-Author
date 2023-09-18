a7c2534051b5d527b7a41d23c6849e7f9dd7da19
Maximilian <scientificresearch309@gmail.com>
2023-09-18 21:06:13 +0200

    some changes applied to user.js

--- models/user.js
+++ models/user.js
@@ -10,33 +10,36 @@ const User = mongoose.model(
       required: true,
       trim: true,
       minlength: 5,
-      maxlength: 255,
+      maxlength: 50,
     },
     email: {
       type: String,
       unique: true,
+      required: true,
+      minlength: 5,
+      maxlength: 255,
     },
     password: {
       type: String,
       required: true,
-      min: 6,
-      max: 255,
+      minlength: 5,
+      maxlength: 1024,
     },
   })
 );
 
 function validateUser(user) {
   const schema = {
-    name: Joi.string().min(5).max(255).required(),
+    name: Joi.string().min(5).max(50).required(),
     // genreId: Joi.objectId().required(),
-    // email: Joi.string().required(),
-    email: Joi.string().email({
-      minDomainSegments: 2,
-      tlds: { allow: ["com", "net"] },
-    }),
-    // password: Joi.string().min(6).required(),
+    email: Joi.string().min(5).max(255).required().email(),
+    // email: Joi.string().email({
+    //   minDomainSegments: 2,
+    //   tlds: { allow: ["com", "net"] },
+    // }),
+    password: Joi.string().min(5).max(255).required(),
     // password: Joi.string().pattern(new RegExp("^[a-zA-Z0-9]{3,30}$")),
-    password: Joi.string().regex(/^[a-zA-Z0-9]{3,30}$/),
+    // password: Joi.string().regex(/^[a-zA-Z0-9]{3,30}$/),
   };
 
   return Joi.validate(user, schema);
