023ce4bdf4daf56765416916a02b11047b02d9e2
Maximilian <scientificresearch309@gmail.com>
2023-09-21 00:48:26 +0200

    Refactoring index.js

--- index.js
+++ index.js
@@ -2,20 +2,14 @@ require("express-async-errors");
 const winston = require("winston");
 require("winston-mongodb");
 // const MongoClient = require("mongodb").MongoClient;
-const error = require("./middleware/error");
 const config = require("config");
 const Joi = require("joi");
 Joi.objectId = require("joi-objectid")(Joi);
 const mongoose = require("mongoose");
-const genres = require("./routes/genres");
-const customers = require("./routes/customers");
-const movies = require("./routes/movies");
-const rentals = require("./routes/rentals");
-const users = require("./routes/users");
-const auth = require("./routes/auth");
 const express = require("express");
 const app = express();
 
+require("./startup/routes")(app);
 // process.on("uncaughtException", (ex) => {
 //   // console.log("WE GOT AN UNCAUGHT EXCEPTION");
 //   winston.error(ex.message, ex);
@@ -99,16 +93,6 @@ mongoose
   })
   .then(() => console.log("Connected to MongoDB..."))
   .catch((err) => console.error("Could not connect to MongoDB..."));
-
-app.use(express.json());
-app.use("/api/genres", genres);
-app.use("/api/customers", customers);
-app.use("/api/movies", movies);
-app.use("/api/rentals", rentals);
-app.use("/api/users", users);
-app.use("/api/auth", auth);
-
-app.use(error);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
