2b609cc3a57ea29a598b98af15ca9ca0e6dbac30
Maximilian <scientificresearch309@gmail.com>
2023-09-24 01:14:05 +0200

    creating our customized DB!

--- startup/db.js
+++ startup/db.js
@@ -1,9 +1,10 @@
 const mongoose = require("mongoose");
 const winston = require("winston");
+const config = require("config");
 
 module.exports = function () {
   mongoose
-    .connect("mongodb://127.0.0.1/vidly", {
+    .connect(config.get("db"), {
       useUnifiedTopology: true,
       useNewUrlParser: true,
       useCreateIndex: true,
