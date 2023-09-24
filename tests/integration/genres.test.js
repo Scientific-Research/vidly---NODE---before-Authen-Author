0d1262201df53324af1f0795c6602fe821477373
Maximilian <scientificresearch309@gmail.com>
2023-09-24 22:23:51 +0200

    first Integration test if valid ID is passed!

--- tests/integration/genres.test.js
+++ tests/integration/genres.test.js
@@ -1,5 +1,6 @@
 const request = require("supertest");
 const { Genre } = require("../../models/genre");
+// const mongoose = require("mongoose");
 let server;
 
 describe("/api/genres", () => {
@@ -22,5 +23,22 @@ describe("/api/genres", () => {
       expect(res.body.some((g) => g.name === "genre1")).toBeTruthy();
       expect(res.body.some((g) => g.name === "genre2")).toBeTruthy();
     });
+  });
+
+  describe("GET /:id", () => {
+    ///////////////////////////////////////////////////////////////////////////////!first Integration Test
+    it("should return a genre if valid id is passed", async () => {
+      const genre = new Genre({
+        // _id: new mongoose.Types.ObjectId().toHexString(),
+        name: "genre1",
+      });
+      await genre.save();
+
+      const res = await request(server).get("/api/genres/" + genre._id);
+      expect(res.status).toBe(200);
+      // expect(res.body).toMatchObject(genre);
+      expect(res.body).toHaveProperty("name", genre.name);
+    });
+    //////////////////////////////////////////////////////////////////////////////////////!first Integration Test
   });
 });
