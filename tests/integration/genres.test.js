0c079f8afae38d18d5dd8f7e6f04eccf1d0c5056
Maximilian <scientificresearch309@gmail.com>
2023-09-25 12:50:57 +0200

    Tests for less than 5 and more than 50 characters are working well!

--- tests/integration/genres.test.js
+++ tests/integration/genres.test.js
@@ -61,38 +61,72 @@ describe("/api/genres", () => {
   });
 
   describe("POST /", () => {
+    // Define the happy path, and then in each test, we change one parameter that clearly aligns with the name of
+    // the test.
+    let name;
+    let token;
+
+    const exec = async () => {
+      return await request(server)
+        .post("/api/genres")
+        .set("x-auth-token", token)
+        // .send({
+        //   // name: "genre1",
+        //   name: name,
+        // });
+        // or in ES6, when the key and value are the same, we can make it short with key or value:
+        .send({
+          // name: "genre1",
+          name,
+        });
+    };
+
+    beforeEach(() => {
+      token = new User().generateAuthToken();
+      name = "genre1";
+    });
+
     // first test : client is not logged in
-    it("should retuen 401 if client is not logged in", async () => {
-      const res = await request(server).post("/api/genres").send({
-        name: "genre1",
-      });
+    it("should return 401 if client is not logged in", async () => {
+      // const res = await request(server).post("/api/genres").send({
+      //   name: "genre1",
+      // });
+      token = "";
+      const res = await exec();
       expect(res.status).toBe(401);
     });
     // second test : client is logged in, but send a genre less than 5 Characters(an invalid character)
     // first we need to logg in, we need to generate an Authentication Token and include that Token in this request(in the header).
     it("should return 400 if genre is less than 5 characters.", async () => {
-      const token = new User().generateAuthToken();
+      // const token = new User().generateAuthToken();
 
-      const res = await request(server)
-        .post("/api/genres")
-        .set("x-auth-token", token)
-        .send({
-          name: "1234",
-        });
+      // const res = await request(server)
+      //   .post("/api/genres")
+      //   .set("x-auth-token", token)
+      //   .send({
+      //     name: "1234",
+      //   });
+
+      name = "1234";
+
+      const res = await exec();
       expect(res.status).toBe(400);
     });
     it("should return 400 if genre is more than 50 characters.", async () => {
-      const token = new User().generateAuthToken();
+      // const token = new User().generateAuthToken();
 
       // to create more than 50 characters => 51 here => we do as following:
-      const nameMoreThan50Characters = new Array(52).join("a");
-      const res = await request(server)
-        .post("/api/genres")
-        .set("x-auth-token", token)
-        .send({
-          // name: "1234oiuefiouweioruijs",
-          name: nameMoreThan50Characters,
-        });
+      // const nameMoreThan50Characters = new Array(52).join("a");
+      name = new Array(52).join("a");
+
+      // const res = await request(server)
+      //   .post("/api/genres")
+      //   .set("x-auth-token", token)
+      //   .send({
+      //     // name: "1234oiuefiouweioruijs",
+      //     name: nameMoreThan50Characters,
+      //   });
+      const res = await exec();
       expect(res.status).toBe(400);
     });
 
@@ -100,12 +134,9 @@ describe("/api/genres", () => {
     it("should save the genre if it is valid.", async () => {
       // const token = new User().generateAuthToken();
 
-      // const res = await request(server)
-      //   .post("/api/genres")
-      //   .set("x-auth-token", token)
-      //   .send({
-      //     name: "genre1",
-      //   });
+      //TODO:
+      // const res = await exec();
+      await exec();
 
       const genre = await Genre.find({ name: "genre1" });
       expect(genre).not.toBeNull();
