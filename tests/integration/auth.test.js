788d1a5a013e2cdaf4206ca81ffe8ab4023ff8f2
Maximilian <scientificresearch309@gmail.com>
2023-09-25 14:45:47 +0200

    test if token is invalid done successfully in auth.test.js using token = "a"

--- tests/integration/auth.test.js
+++ tests/integration/auth.test.js
@@ -23,10 +23,17 @@ describe("auth middleware", () => {
   beforeEach(() => {
     token = new User().generateAuthToken();
   });
-
+  // return 401 error when there is no
   it("should return 401 if no token is provided!", async () => {
-    token = null;
+    token = "";
     const res = await exec();
     expect(res.status).toBe(401);
+  });
+
+  // return 400 eroor when token is invalid.
+  it("should return 400 if token is invalid.!", async () => {
+    token = "a";
+    const res = await exec();
+    expect(res.status).toBe(400);
   });
 });
