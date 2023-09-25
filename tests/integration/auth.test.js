bd31c30e577aecb6ff0815580b19bc8a71735e3f
Maximilian <scientificresearch309@gmail.com>
2023-09-25 14:22:49 +0200

    There is not token,the Test passed!

--- tests/integration/auth.test.js
+++ tests/integration/auth.test.js
@@ -1,3 +1,4 @@
+const { User } = require("../../models/user");
 const request = require("supertest");
 
 describe("auth middleware", () => {
@@ -10,8 +11,22 @@ describe("auth middleware", () => {
     // await Genre.remove({});
   });
 
+  let token;
   // using MH happy path:
-  const exec = () => {};
+  const exec = () => {
+    return request(server)
+      .post("/api/genres")
+      .set("x-auth-token", token)
+      .send({ name: "genre1" });
+  };
 
-  it("should return 401 if no token is provided!", () => {});
+  beforeEach(() => {
+    token = new User().generateAuthToken();
+  });
+
+  it("should return 401 if no token is provided!", async () => {
+    token = "";
+    const res = await exec();
+    expect(res.status).toBe(401);
+  });
 });
