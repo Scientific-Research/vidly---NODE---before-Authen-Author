f2f3421198c569ef0dfee1f444935ca74227a71d
Maximilian <scientificresearch309@gmail.com>
2023-09-29 23:09:48 +0200

    in TDD, we have to write the simplest code to detect the failure like here, we don't need to get the token , ...

--- routes/returns.js
+++ routes/returns.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
-router.post("/", async (req, res) => {});
+router.post("/", async (req, res) => {
+  res.status(401).send("Unauthorized User!");
+});
 
 module.exports = router;
