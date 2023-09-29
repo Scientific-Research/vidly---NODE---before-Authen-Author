42d9c8020fa952c788fd5464d028086bd2676284
Maximilian <scientificresearch309@gmail.com>
2023-09-29 15:08:40 +0200

    till now, we defined the rental object and customer as first property in beforeEach

--- tests/integration/returns.test.js
+++ tests/integration/returns.test.js
@@ -1,13 +1,27 @@
 // like always we start with Test Suits => describe()
-let server;
+const { Rental } = require("../../models/rental");
+const mongoose = require("mongoose");
 
 describe("/api/returns", () => {
+  let server;
+  let customerId;
+
   beforeEach(() => {
     server = require("../../index");
+
+    customerId = mongoose.Types.ObjectId();
+    const rental = new Rental({
+      // two properties
+      customer: {
+        _Id: customerId,
+        name: "12345", // name has to have at least 5 characters. minlength:5 and maxlength: 50
+        phone: "12345", // phone has to have at least 5 characters. minlength:5 and maxlength: 50
+      },
+    });
   });
 
   afterEach(async () => {
-    await server.close();
-    await Genre.deleteMany({});
+    server.close();
+    // await Genre.deleteMany({});
   });
 });
