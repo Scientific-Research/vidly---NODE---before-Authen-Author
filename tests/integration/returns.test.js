d416dc74a0714e5d50d533894eb83adb5c1f1dce
Maximilian <scientificresearch309@gmail.com>
2023-09-29 15:14:44 +0200

    I just added the second property as movie to the rental!

--- tests/integration/returns.test.js
+++ tests/integration/returns.test.js
@@ -1,21 +1,31 @@
 // like always we start with Test Suits => describe()
 const { Rental } = require("../../models/rental");
+const { Movie } = require("../../models/movie");
+
 const mongoose = require("mongoose");
 
 describe("/api/returns", () => {
   let server;
   let customerId;
+  let movieId;
 
   beforeEach(() => {
     server = require("../../index");
 
     customerId = mongoose.Types.ObjectId();
+    movieId = mongoose.Types.ObjectId();
+
     const rental = new Rental({
       // two properties
       customer: {
         _Id: customerId,
         name: "12345", // name has to have at least 5 characters. minlength:5 and maxlength: 50
         phone: "12345", // phone has to have at least 5 characters. minlength:5 and maxlength: 50
+      },
+      movie: {
+        _id: movieId,
+        title: "movie_title", // title has to have at least 5 characters. minlength:5 and maxlength: 50
+        dailyRentalRate: 2,
       },
     });
   });
